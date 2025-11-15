b670973a893603cb10ab6009b3d6dd93b970c50a
ali-heydari-py <ahey6715@gmail.com>
2025-11-15 22:15:36 +0330

    bckend new version

--- startcode1.bat
+++ startcode1.bat
@@ -11,9 +11,13 @@ python manage.py migrate
 echo 🖼️ Collecting static files...
 python manage.py collectstatic --noinput
 
-echo 🐍 Seeding static categories via shell...
+echo 🐍 Seeding static categories, products, and collections via shell...
 echo from inner.seed_static_categories import seed_static_categories > temp_seed.py
+echo from inner.signals import seed_default_products, seed_collections >> temp_seed.py
 echo seed_static_categories() >> temp_seed.py
+echo seed_default_products() >> temp_seed.py
+echo seed_collections() >> temp_seed.py
+echo exit() >> temp_seed.py
 
 python manage.py shell < temp_seed.py
 del temp_seed.py
@@ -23,4 +27,3 @@ openssl req -x509 -newkey rsa:4096 -keyout localhost.key -out localhost.crt -day
 
 echo 🌐 Starting server with SSL...
 python manage.py runserver_plus --cert-file localhost.crt --key-file localhost.key
-
